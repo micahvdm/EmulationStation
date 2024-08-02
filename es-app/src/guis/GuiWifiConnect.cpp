15afe4f7f3edc54f5d68a34852049e2291848e6c
Micah John <micahvdm@gmail.com>
2024-08-02 11:29:13 +0200

    fix errors

--- es-app/src/guis/GuiWifiConnect.cpp
+++ es-app/src/guis/GuiWifiConnect.cpp
@@ -128,11 +128,11 @@ GuiWifiConnect::GuiWifiConnect(Window* window, std::string wifiName, bool encryp
 		}
 		mWindow->pushGui(new GuiMsgBox(mWindow, "Network: " + wifiName + "\n Password: " + ed->getValue(), "Connect", [this, wifiName, ed] {
 			// Quick and dirty just send the info to wificonnect
-			std::string cmdStr = "sudo " + getHomePath() + "/.emulationstation/app/wifi/./wificonnect --ssid '" + wifiName + "' --password " + ed->getValue();
+			std::string cmdStr = "sudo " + Utils::FileSystem::getHomePath + "/.emulationstation/app/wifi/./wificonnect --ssid '" + wifiName + "' --password " + ed->getValue();
 			const char* cmd = cmdStr.c_str();
 
 			// Make sure wificonnect exists
-			std::string path = getHomePath() + "/.emulationstation/app/wifi/wificonnect";
+			std::string path = Utils::FileSystem::getHomePath + "/.emulationstation/app/wifi/wificonnect";
 			if (boost::filesystem::exists(path)) {
 				system(cmd);
 				mConnected = true;
